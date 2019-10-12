9ba6025cf6859e8966ebcaf225dfbf5d92baebd5
jadyjacques <jadyjacques.nave@gmail.com>
2019-10-11 22:16:43 -0300

    exercicios de comunicação com front, requisições, filtro por id, exclusão de cpf do retorno e inclusao da pasta front

--- src/app.js
+++ src/app.js
@@ -10,6 +10,15 @@ const index = require("./routes/index")
 const alunas = require('./routes/alunasRoute')
 const professoras = require('./routes/professorasRoute')
 
+app.use(function(req, res, next) { //resolvendo problema de CORS
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+})
+
 app.use('/', index)
 app.use("/alunas", alunas)
 app.use("/professoras", professoras)
--- src/routes/professorasRoute.js
+++ src/routes/professorasRoute.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/professorasController')
 
 
 router.get("/", controller.get)
-router.get("/:signo", controller.getBySigno) //procurar o objeto professora atraves do signo
+// router.get("/:signo", controller.getBySigno) //procurar o objeto professora atraves do signo
+router.get("/:id", controller.getById)
 
 module.exports = router
